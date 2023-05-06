14285acfc5d4c57d5fc7c235d0322edcf67295e3
Zhenya <shaynurov75@gmail.com>
2023-05-06 20:44:22 +0900

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int balans = 100;
-        int sumReple = 1000;
+        int sumReple = 1100;
         int total;
         int bonus;
         if (sumReple >= 1000) {
