4bc3991bceecb5e72710cfbacafbe325f696961c
Zhenya <shaynurov75@gmail.com>
2023-05-06 20:38:28 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int balans = 100;
+        int sumReple = 1100;
+        int total;
+        int bonus;
+        if (sumReple > 1000) {
+            bonus = sumReple / 100;
+            total = balans + sumReple + bonus;
+            System.out.println("Количество бонусных рублей = " + bonus + " р " + "Итоговый счет = " + total + " р ");
+        } else {
+            bonus = 0;
+            total = balans + sumReple + bonus;
+            System.out.println("Количество бонусных рублей = " + bonus + " р " + "Итоговый счет = " + total + " р ");
+        }
+    }
+}
